d94a944c70693faf1032471f925c8662f52ae995
suyogmgr <mgrsuyog6@gmail.com>
2025-03-19 09:48:19 +0545

    updatelinks

--- Ticket-booking/sign in/db.php
+++ Ticket-booking/sign in/db.php
@@ -1,8 +1,8 @@
 <?php
-    $host = 'sql101.infinityfree.com';
-    $user = 'if0_38546730';
-    $pass = 'mpYN9DiSdtViI0';
-    $db_name = 'if0_38546730_movies';
+    $host = 'localhost';
+    $user = 'rana';
+    $pass = '@Suyog123';
+    $db_name = 'movie';
     $conn = '';
 
     $conn = mysqli_connect($host, $user, $pass, $db_name);
